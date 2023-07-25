724f78cd2064c5bb73d2389d2600decdef773d48
aeross <andrewpratama22@gmail.com>
2023-07-25 21:57:29 +1000

    completed 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
+const add = function(n1, n2) {
+  return n1 + n2;
 	
 };
 
-const subtract = function() {
+const subtract = function(n1, n2) {
+  return n1 - n2;
 	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  var sumOutput = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sumOutput += arr[i];
+  }
+  return sumOutput;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  var multOutput = 1;
+  for (var i = 0; i < arr.length; i++) {
+    multOutput *= arr[i];
+  }
+  return multOutput;
 };
 
-const power = function() {
-	
+const power = function(n, p) {
+  var output = 1;
+	while (p > 0) {
+    output *= n;
+    p--;
+  }
+  return output;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  var output = 1;
+  while (n > 0) {
+    output *= n;
+    n--;
+  }
+	return output;
 };
 
 // Do not edit below this line
