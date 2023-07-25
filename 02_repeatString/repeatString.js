fc80e6262482a5ea5ad768d9a56dfcacac333329
aeross <andrewpratama22@gmail.com>
2023-07-25 16:51:10 +1000

    Completed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,6 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    return string * num;
 };
-
+console.log(repeatString("Ho", 5));
 // Do not edit below this line
 module.exports = repeatString;
