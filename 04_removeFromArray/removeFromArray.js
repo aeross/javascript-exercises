c60b7d753d3e3806f04a8422e0c1790ee7db3bf4
aeross <andrewpratama22@gmail.com>
2023-07-25 17:12:37 +1000

    Completed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
+    var output = arguments[0];
 
+    for (var i = 0; i < arguments.length; i++) {
+        var j = output.indexOf(arguments[i]);
+        if (j !== -1) {
+            output.splice(j, 1);
+        }
+    }
+
+    return output;
 };
 
 // Do not edit below this line
