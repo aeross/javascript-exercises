6575655045fe2542c4733dabf1bd02fd12f96b05
aeross <andrewpratama22@gmail.com>
2023-07-25 17:01:52 +1000

    Completed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
 const repeatString = function(string, num) {
-    return string * num;
+    if (num < 0) {
+        return "ERROR";
+    }
+
+    let output = "";
+    while (num > 0) {
+        output += string;
+        num--;
+    }
+    return output;
 };
 console.log(repeatString("Ho", 5));
 // Do not edit below this line
