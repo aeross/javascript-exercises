5513146110df784ec51a57415627eb927fe78c73
aeross <andrewpratama22@gmail.com>
2023-07-25 21:46:21 +1000

    Completed 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
 const convertToCelsius = function() {
+  var farenheit = arguments[0];
+  return Math.round((farenheit - 32) * (50/9)) / 10;
 };
 
 const convertToFahrenheit = function() {
+  var celcius = arguments[0];
+  return Math.round(((celcius * 9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
